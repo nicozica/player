cf21eb3f4d8e806b2e1b3508675faa4fd913f63e
Nicolás Zicarelli <nico@argensonix.com.ar>
2014-10-27 20:11:29 -0300

    V5 Frontend only

--- players/player_radiouno.php
+++ players/player_radiouno.php
@@ -9,14 +9,19 @@
 			<img src="assets/images/ban_rollingstones.jpg" />
 		</div>
 	</div><!-- /.player-box -->
-	<div class="icon-players">
-		<p>Reproductores</p>
-		<ul>
-			<li><a class="wmp" href="assets/streams/radiounodigital.asx">Windows Media Player</a></li>
-			<li><a class="winamp" href="assets/streams/radiounodigital.pls">Winamp</a></li>
-			<li><a class="itunes" href="assets/streams/radiounodigital.pls">iTunes</a></li>
-			<li><a class="realplayer" href="assets/streams/radiounodigital.ram">Real Player</a></li>
-		</ul>
+	<div class="second-line">
+		<div class="icon-players">
+			<p>Reproductores</p>
+			<ul>
+				<li><a class="wmp" href="assets/streams/radiounodigital.asx">Windows Media Player</a></li>
+				<li><a class="winamp" href="assets/streams/radiounodigital.pls">Winamp</a></li>
+				<li><a class="itunes" href="assets/streams/radiounodigital.pls">iTunes</a></li>
+				<li><a class="realplayer" href="assets/streams/radiounodigital.ram">Real Player</a></li>
+			</ul>
+		</div>
+		<div class="listeners">
+			<p>Oyentes online <span>481</span></p>
+		</div>
 	</div>
 	<div id="rds_radiouno">
 
